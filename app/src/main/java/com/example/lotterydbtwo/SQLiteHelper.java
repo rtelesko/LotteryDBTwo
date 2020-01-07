c70c7cf2066c6b3780dbcb6c452496a5a7a45aeb
rainer <rainer.telesko@fhnw.ch>
2020-01-07 09:10:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lotterydbtwo/SQLiteHelper.java
@@ -0,0 +1,38 @@
+package com.example.lotterydbtwo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+
+    static String DATABASE_NAME="PlayersDataBase";
+
+    public static final String TABLE_NAME="Players";
+    public static final String Table_Column_ID="id";
+    public static final String Table_Column_1_Name="name";
+    public static final String Table_Column_2_Location ="location";
+
+
+    public SQLiteHelper(Context context) {
+
+        super(context, DATABASE_NAME, null, 1);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+
+        String CREATE_TABLE="CREATE TABLE IF NOT EXISTS "+TABLE_NAME+" ("+Table_Column_ID+" INTEGER PRIMARY KEY, "+Table_Column_1_Name+" VARCHAR, "+ Table_Column_2_Location +" VARCHAR)";
+        database.execSQL(CREATE_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+
+    }
+
+}
